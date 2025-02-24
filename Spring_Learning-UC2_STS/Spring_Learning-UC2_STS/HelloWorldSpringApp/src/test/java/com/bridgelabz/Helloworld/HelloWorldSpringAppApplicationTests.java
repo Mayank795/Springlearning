4242f64016f9c19d2522e48be8754562eeaaf318
Mayank795 <mayankspal555@gmail.com>
2025-02-24 15:05:38 +0530

    [mayank]-add uc1

---
+++ Spring_Learning-UC2_STS/Spring_Learning-UC2_STS/HelloWorldSpringApp/src/test/java/com/bridgelabz/Helloworld/HelloWorldSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
